0b97ec6b024fdedb380376971735efee445b9202
Roman <0knils0@gmail.com>
2026-07-16 12:14:14 +0500

    Logs clean up

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,21 +4,13 @@ import path from 'path';
 import dotenv from 'dotenv';
 
 const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
-
-console.log('__dirname:', __dirname);
-
 const envPath = path.resolve(__dirname, '../../../', envFile);
 
-console.log('env path:', envPath);
-
 dotenv.config({ path: envPath });
-
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  console.log('DATABASE_URL =', process.env.DATABASE_URL);
   await app.listen(3000);
 }
 bootstrap();
