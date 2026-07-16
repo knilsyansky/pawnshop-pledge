cd51d666baa3924b13b66aa0fde51b6578603618
Roman <0knils0@gmail.com>
2026-07-16 11:50:10 +0500

    Fresh build fix

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  console.log('DATABASE_URL =', process.env.DATABASE_URL);
   await app.listen(3000);
 }
 bootstrap();
