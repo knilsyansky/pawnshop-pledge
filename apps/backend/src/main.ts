c825d6d96f10473188d55349c5e5798235fdd8f6
Roman <0knils0@gmail.com>
2026-07-16 12:11:50 +0500

    Fresh build fix

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,7 +4,16 @@ import path from 'path';
 import dotenv from 'dotenv';
 
 const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
-dotenv.config({ path: path.resolve(__dirname, '../../', envFile) });
+
+console.log('__dirname:', __dirname);
+
+const envPath = path.resolve(__dirname, '../../../', envFile);
+
+console.log('env path:', envPath);
+
+dotenv.config({ path: envPath });
+
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
